8a9b4c191f29a0de81d1b914e12208a95f87b17e
Massimo Cantú <massimocantugarcia@gmail.com>
2024-07-12 19:20:12 -0300

    Made dendro brewing code more efficient

--- src/main/java/com/ferreusveritas/dynamictrees/util/DendroBrewingRecipe.java
+++ src/main/java/com/ferreusveritas/dynamictrees/util/DendroBrewingRecipe.java
@@ -2,9 +2,6 @@ package com.ferreusveritas.dynamictrees.util;
 
 import com.ferreusveritas.dynamictrees.item.DendroPotion;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.common.brewing.IBrewingRecipe;
 
 /**
@@ -12,60 +9,27 @@ import net.minecraftforge.common.brewing.IBrewingRecipe;
  *
  * @author Harley O'Connor
  */
-public final class DendroBrewingRecipe implements IBrewingRecipe {
+public record DendroBrewingRecipe(ItemStack input, ItemStack ingredient, ItemStack output) implements IBrewingRecipe {
 
-    private final ItemStack input;
-    private final ItemStack ingredient;
-    private final ItemStack output;
-
-    public DendroBrewingRecipe(final ItemStack input, final ItemStack ingredient, final ItemStack output) {
-        this.input = input;
-        this.ingredient = ingredient;
-        this.output = output;
-    }
-
-    @Override
-    public boolean isInput(final ItemStack inputStack) {
-	// Only allow Potion.AWKWARD or DendroPotion and for DendroPotion.TRANSFORM, only if it doesn't already have a tree tag.
-	if ((!inputStack.getOrCreateTag().contains(DendroPotion.TREE_TAG_KEY) && inputStack.getOrCreateTag().contains(DendroPotion.INDEX_TAG_KEY)) || inputStack.getItem() == Items.POTION) {
-		return true;
-	}
-		return false;
+	@Override
+	public boolean isInput(final ItemStack inputStack) {
+		return ItemStack.isSameItemSameTags(input, inputStack);
 	}
 
-    @Override
-    public boolean isIngredient(final ItemStack ingredientStack) {
-	    return this.ingredient.getItem().equals(ingredientStack.getItem());
-    }
+	@Override
+	public boolean isIngredient(final ItemStack ingredientStack) {
+		return ItemStack.isSameItemSameTags(ingredient, ingredientStack);
+	}
 
-    @Override
-    public ItemStack getOutput(final ItemStack inputStack, final ItemStack ingredientStack) {
-	    // We need to apply logic for the brewing or simply the ingredient defines the output and any input was allowed
-	    // A smarter way would be nice, but it works
-	    if (!inputStack.isEmpty() && !ingredientStack.isEmpty() && isIngredient(ingredientStack) && isInput(inputStack)) {
-		    if (ingredientStack.is(Items.CHARCOAL) & PotionUtils.getPotion(inputStack) == Potion.byName("awkward")) {
-			    return this.output.copy();
-		    }
-		    if (ingredientStack.is(Items.CHARCOAL) | inputStack.getItem() == Items.POTION) {
-			    return ItemStack.EMPTY;
-		    }
-		    if ((ingredientStack.is(Items.SLIME_BALL) || ingredientStack.is(Items.PUMPKIN_SEEDS) || ingredientStack.is(Items.GHAST_TEAR) || ingredientStack.is(Items.PRISMARINE_CRYSTALS)) & DendroPotion.getPotionType(inputStack) == DendroPotion.DendroPotionType.BIOCHAR) {
-			    return this.output.copy();
-		    }
-		    if ((ingredientStack.is(Items.SLIME_BALL) || ingredientStack.is(Items.PUMPKIN_SEEDS) || ingredientStack.is(Items.GHAST_TEAR) || ingredientStack.is(Items.PRISMARINE_CRYSTALS)) | DendroPotion.getPotionType(inputStack) != DendroPotion.DendroPotionType.TRANSFORM) {
-			    return ItemStack.EMPTY;
-		    }
-		    return this.output.copy();
-	    }
-	    return ItemStack.EMPTY;
-    }
-	public ItemStack getInput() {
-		return input;
+	@Override
+	public ItemStack getOutput(final ItemStack inputStack, final ItemStack ingredientStack) {
+		// We need to apply logic for the brewing or simply the ingredient defines the output and any input was allowed
+		// A smarter way would be nice, but it works
+		if (!inputStack.isEmpty() && !ingredientStack.isEmpty() && isIngredient(ingredientStack) && isInput(inputStack)) {
+			return this.output.copy();
+		}
+		return ItemStack.EMPTY;
 	}
-	public ItemStack getIngredient() {
-		return ingredient;
-	}
-	public ItemStack getOutput() {
-		return output;
-	}
+
+
 }
